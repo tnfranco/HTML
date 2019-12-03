dfd0be621e966b4592fea1439ce306c50b1b23e4
Tiago Franco <57368762+tnfranco@users.noreply.github.com>
2019-12-02 22:39:52 -0300

    Create Multiplicar.py

---
+++ Java/Multiplicar.py
@@ -0,0 +1,9 @@
+
+
+num1 = int(input("Informe o primeiro numero: "))
+num2 = int(input("Informe o segundo numero: "))
+
+soma = num1 * num2;
+multiplicacao  = soma * num1
+
+print("O resultado da multiplicação é {0}".format(multiplicacao))
