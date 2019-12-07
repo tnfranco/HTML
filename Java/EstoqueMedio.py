475887cb50aaf2f461ff74c4ce612f125fe0c49c
Tiago Franco <57368762+tnfranco@users.noreply.github.com>
2019-12-06 22:04:03 -0300

    Create EstoqueMedio.py

---
+++ Java/EstoqueMedio.py
@@ -0,0 +1,9 @@
+# coding: iso-8859-1 -*-
+quantidade_maxima = int(input("Informe a quantidade maxima"))
+quantidade_minima = int(input("Informe a quantidade minima"))
+
+
+EstoqueMedio = (quantidade_minima + quantidade_maxima) / 2 
+
+
+print("O estoque medio é {0}".format(EstoqueMedio))
