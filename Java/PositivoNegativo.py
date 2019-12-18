59b21fa990e455a0e5a4b929173f65c579e7d936
Tiago Franco <57368762+tnfranco@users.noreply.github.com>
2019-12-17 23:18:06 -0300

    Create PositivoNegativo.py

---
+++ Java/PositivoNegativo.py
@@ -0,0 +1,14 @@
+# coding: iso-8859-1 -*-
+
+numero = int(input("Informe um numero"))
+
+if numero % 2 == 0:
+   if numero > 0:
+       print("O numero {0} é par e positivo".format(numero))
+     else:
+        print("O numero {0} é par e negativo".format(numero)
+else:
+    if numero > 0:
+        print("O numero {0} é impar e positivo".format(numero))
+    else:
+        print("O numero {0} é impar e negativo ".format(numero)))
