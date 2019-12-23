3fe23248ff83f372e7eff987254a40e3d13aae99
Tiago Franco <57368762+tnfranco@users.noreply.github.com>
2019-12-22 21:57:29 -0300

    Delete Login.py

--- Login.py
+++
@@ -1,6 +0,0 @@
-usuario = input("Digite o nome do seu usuario: ")
-senha = input("Digite sua senha: ")
-while senha == usuario:
-    print("Senha nao pode ser igual ao usuario")
-    usuario = input("Digite o nome do seu usuario: ")
-    senha = input("Digite sua senha: ")
