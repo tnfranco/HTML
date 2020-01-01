eed6667518b65ce0d6c16a8a2bd58859e3697f38
Tiago Franco <57368762+tnfranco@users.noreply.github.com>
2020-01-01 20:00:41 -0300

    Create vet.py

---
+++ Java/vet.py
@@ -0,0 +1,13 @@
+vetor = []
+codigo = int(input("Digite um valor"))
+if codigo != 0:
+    for n in range (0, 5):
+        num = float(input("Informe um valor real: "))
+        vetor.append(num)
+    if  codigo == 1:
+        for n in vetor:
+            print(n)
+    if codigo == 2:
+        vetor.reverse()
+        for n in vetor:
+            print(n)
